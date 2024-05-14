f0600b9792d94806211294bfd686de4296eeb658
carpediem365 <136786274+carpediem365@users.noreply.github.com>
2024-05-14 11:36:59 +0900

    Configure JawsDB MySQL

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2/promise');
 
+// const dbConfig = {
+//     host: '127.0.0.1',
+//     port: 3306,
+//     user: 'root',
+//     password: '0000',
+//     database: 'KakaoTalkDB'
+// };
+
 const dbConfig = {
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '0000',
-    database: 'KakaoTalkDB'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 };
 
 const connect = async () => {
